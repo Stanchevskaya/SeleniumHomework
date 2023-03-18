cd94fb4e97836af684350732f1b8da97faac19e3
Stanchevskaya <stanchevskaya@gmail.com>
2023-03-18 20:20:58 +0200

    new

---
+++ src/test/java/app/tests/WebDriverTests.java
@@ -0,0 +1,16 @@
+package app.tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverTests {
+
+    @Test
+    public void testGetUrl() {
+        System.setProperty("webdriver.chrome.driver", "src/test/esources/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ray-ban.com/");
+    }
+}
