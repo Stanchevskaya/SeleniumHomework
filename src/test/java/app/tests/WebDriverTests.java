73c2b00430c78459f270c0eca921c7edf58d5dbc
Stanchevskaya <stanchevskaya@gmail.com>
2023-03-29 14:12:37 +0300

    new

--- src/test/java/app/tests/WebDriverTests.java
+++ src/test/java/app/tests/WebDriverTests.java
@@ -1,16 +1,56 @@
 package app.tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverTests {
 
-    @Test
-    public void testGetUrl() {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         System.setProperty("webdriver.http.factory", "jdk-http-client");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.ray-ban.com/");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
+
+    @Test
+    public void testGetUrl() {
+        driver.navigate().to("https://www.notino.bg/");
+        String expected = "Козметика и парфюми онлайн | Парфюмерия notino.bg";
+        Assertions.assertEquals(driver.getTitle(), expected);
+        Assertions.assertTrue(driver.getCurrentUrl().contains("notino"));
+    }
+
+@Test
+    public void testInvalidCredentials() {
+    driver.navigate().to("https://www.notino.bg/account/login");
+
+    WebElement emailInput = driver.findElement(By.xpath("//input[@name='Email']"));
+    emailInput.sendKeys("testdkdlas@blabla.com");
+
+    WebElement passInput = driver.findElement(By.xpath("//input[@id='Password']"));
+    passInput.sendKeys("tdhjkd");
+
+    WebElement loginButton = driver.findElement(By.xpath("//button[@name='button']"));
+    loginButton.click();
+
+    String actualError = driver.findElement(By.xpath("//*[@id=\"login\"]/div/fieldset/form/fieldset/p[5]/span")).getText();
+    String expectedError = "Невалиден вход – неправилен имейл или парола.";
+
+    Assertions.assertEquals(actualError, expectedError);
+    driver.close();
+    }
+
+@AfterAll
+    static void close () {
+        driver.quit();
+    }
+
 }
+
+
